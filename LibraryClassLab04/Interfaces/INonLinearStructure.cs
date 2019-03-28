860f59afc80a64e4a0fc6d368b8da8641a37a5b1
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-03-27 21:23:24 -0600

    Modifying  BTree Files
    Modifying BTree Files

---
+++ LibraryClassLab04/Interfaces/INonLinearStructure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryClassLab04.Arbol_B;
+
+namespace LibraryClassLab04.Interfaces
+{
+    interface INonLinearStructure<T>
+    {
+        void Insertar(ref Node<T> arbol, T value, Delegate comparer, Node<T> padre);
+        T Mostrar();
+        bool Buscar(Node<T> arbol, T value, Delegate comparer);
+        T DevolverValor();
+        void Eliminar(Node<T> Arbol, T value, Delegate comparer);
+    }
+}
